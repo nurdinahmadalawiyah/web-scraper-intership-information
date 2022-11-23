c3e4eef33c1619244a4af85d9534cd85ae86ba06
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2022-11-23 21:46:06 +0700

    Change JSON response

--- src/scrapeGlintsHandler.js
+++ src/scrapeGlintsHandler.js
@@ -1,5 +1,6 @@
-const RequestPromise = require("request-promise"),
-  Cheerio = require("cheerio");
+const { nanoid } = require("nanoid");
+const RequestPromise = require("request-promise");
+const Cheerio = require("cheerio");
 
 const getGlintsData = async () => {
   const uri = "https://glints.com/id/en/opportunities/jobs/explore?keyword=Informatika%2C+Programmer&country=ID&searchCity=29367&locationName=Bandung%2C+Indonesia&jobTypes=INTERNSHIP";
@@ -12,19 +13,25 @@ const getGlintsData = async () => {
   };
 
   const $ = await RequestPromise(options);
-  let dataScraping = [];
+  
+  let data = [];
+  
   $(
     "div[class='JobCardsc__JobcardContainer-sc-1f9hdu8-0 hvpJwO CompactOpportunityCardsc__CompactJobCardWrapper-sc-1y4v110-0 dLzoMG compact_job_card']"
-  ).each(function (_i, _elem) {
-    let data = {
+    ).each(function () {
+    let glints = {
+      id: nanoid(16),
       position: $(this).find("h2[class='CompactOpportunityCardsc__JobTitle-sc-1y4v110-7 cYZbCX']").text().trim(),
       company: $(this).find("a[class='CompactOpportunityCardsc__CompanyLink-sc-1y4v110-8 kCzMph']").text().trim(),
       image: $(this).find("img[src]").attr("src"),
       link: $(this).find("a[href]").attr("href"),
     };
-    dataScraping.push(data);
+    data.push(glints)
   });
-  return dataScraping;
+  return {
+    status: 'success',
+      data,
+  };
 };
 
 module.exports = {
--- src/scrapeProspleHandler.js
+++ src/scrapeProspleHandler.js
@@ -1,5 +1,6 @@
-const RequestPromise = require("request-promise"),
-  Cheerio = require("cheerio");
+const { nanoid } = require("nanoid");
+const RequestPromise = require("request-promise");
+const Cheerio = require("cheerio");
 
 const getProspleData = async () => {
   const uri = "https://id.prosple.com/search-jobs?opportunity_types=2&locations=9714%2C9714%7C24768&defaults_applied=1&study_fields=502";
@@ -12,9 +13,14 @@ const getProspleData = async () => {
   };
 
   const $ = await RequestPromise(options);
-  let dataScraping = [];
-  $("li[class='SearchResultsstyle__SearchResult-sc-c560t5-1 hlOmzw']").each(function (_i, _elem) {
-    let data = {
+
+  let data = [];
+
+  $(
+    "li[class='SearchResultsstyle__SearchResult-sc-c560t5-1 hlOmzw']"
+    ).each(function () {
+    let prosple = {
+      id: nanoid(16),
       position: $(this).find("a[class='JobTeaserstyle__JobTeaserTitleLink-sc-1p2iccb-2 eiICbF']").text().trim(),
       description: $(this).find("div[data-testid='raw-html']").text().trim(),
       company: $(this).find("header[class='Teaser__TeaserHeader-sc-129e2mv-1 JobTeaserstyle__JobTeaserHeader-sc-1p2iccb-1 iBnwQU bycdHT']").text().trim(),
@@ -22,9 +28,12 @@ const getProspleData = async () => {
       location: $(this).find("div[class='sc-gsTCUz JobTeaserstyle__JobLocation-sc-1p2iccb-8 hAURsc jOLgFK']").text().trim(),
       link: $(this).find("a[href]").attr("href"),
     };
-    dataScraping.push(data);
+    data.push(prosple);
   });
-  return dataScraping;
+  return {
+    status: 'success',
+    data,
+  }
 };
 
 module.exports = {
