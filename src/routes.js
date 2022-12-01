157622c285af9a1603c7ddc4c57ad3607a853668
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2022-12-01 21:11:13 +0700

    test

--- src/routes.js
+++ src/routes.js
@@ -27,7 +27,7 @@ const routes = [
     method: "*",
     path: "/{any*}",
     handler: (request, h) => {
-      ScrapeGlintsHandler.getGlintsData()
+      return "Halaman tidak ditemukan";
     },
   },
 ];
